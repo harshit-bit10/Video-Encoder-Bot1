4ae048afca01423a878fd4a93d395e074ca0965d
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-01-28 20:24:57 +0530

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -47,7 +47,7 @@ async def start_message(app, message):
     if not c:
         return
     await AddUserToDatabase(app, message)
-    text = f"Hi {message.from_user.mention()}<a href='https://telegra.ph/file/11379aba315ba245ebc7b.jpg'>!</a> I'm VideoEncoder Bot which will do magic with your file."
+    text = f"Hi {message.from_user.mention()}<a href='https://telegra.ph/shvb1-01-27'>!</a> I'm VideoEncoder Bot which will do magic with your file.\n<b>Made with Love By @SupremeYoriichi</b>"
     await message.reply(text=text, reply_markup=start_but)
 
 
@@ -84,7 +84,7 @@ For Owner:
 - /addchat and /addsudo
 - /rmsudo and /rmchat
 
-Supports: <a href='https://telegra.ph/Supports-03-29'>click here</a>"""
+Supports: <a href='https://telegra.ph/SharkToonsIndia-Video---Encoder-Bot-Supports-01-28'>click here</a>"""
     await message.reply(text=msg, disable_web_page_preview=True, reply_markup=start_but)
 
 
